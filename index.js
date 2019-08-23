b8fa82c2638f243fce7c1f49b71a2060429d54d1
aurimas535 <aurimasmickys@gmail.com>
2019-08-23 09:08:48 +0300

    fix is mobile

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 import { Dimensions, PixelRatio, Platform } from 'react-native';
 
 const { userAgent } = navigator;
-
-const IS_TIZEN = /Tizen|tizen|TIZEN/.test(userAgent);
-const IS_WEB_OS = /Web0S|web0s|WEB0S|webos|WEBOS|WebOS/.test(userAgent);
-const IS_STV = IS_TIZEN || IS_WEB_OS;
-const IS_WEB = Platform.OS === 'web' && !IS_STV;
-const IS_TV = Platform.isTV || IS_TIZEN || IS_WEB_OS;
-const IS_MOBILE = (Platform.OS === 'android' || Platform.OS === 'ios') && !IS_TV;
-const IS_ANDROID_TV = Platform.OS === 'android' && IS_TV;
-const IS_TV_OS = Platform.OS === 'ios' && IS_TV;
-const IS_ANDROID = Platform.OS === 'android' && !IS_TV;
-const IS_IOS = Platform.OS === 'ios' && !IS_TV;
-
 const isTablet = () => {
   const pixelDensity = PixelRatio.get();
 
@@ -33,14 +21,14 @@ const getOS = () => {
 
   if ((IS_MOBILE && IS_ANDROID) || (userAgent && userAgent.match(/Android/))) os = "android";
   else if ((IS_MOBILE && IS_IOS) || (userAgent && userAgent.match(/iPhone|iPad|iPod/))) os = "ios";
-  else if (userAgent && userAgent.match(/Windows Phone/)) os = "windowsPhone";
-  else if (userAgent && userAgent.match(/BlackBerry/)) os = "blackBerry";
+  else if (userAgent && userAgent.match(/Windows Phone/)) os = "windowsphone";
+  else if (userAgent && userAgent.match(/BlackBerry/)) os = "blackberry";
   else if (IS_ANDROID_TV || userAgent && userAgent.match(/Android TV/)) os = "androidtv";
   else if (IS_TV_OS || userAgent && userAgent.match(/AppleTV/i)) os = "tvos";
   else if (IS_TIZEN) os = "tizen";
   else if (IS_WEB_OS) os = "webos";
   else if (userAgent && userAgent.match("Windows")) os = "windows";
-  else if (userAgent && userAgent.match("Mac")) os = "macOS";
+  else if (userAgent && userAgent.match("Mac")) os = "macos";
   else if (userAgent && userAgent.match("X11")) os = "unix";
   else if (userAgent && userAgent.match("Linux")) os = "linux";
   else os = "unknown";
@@ -64,10 +52,22 @@ const getFormFactor = () => {
   return 'unknown';
 };
 
+const IS_TIZEN = /Tizen|tizen|TIZEN/.test(userAgent);
+const IS_WEB_OS = /Web0S|web0s|WEB0S|webos|WEBOS|WebOS/.test(userAgent);
+const IS_MOBILE = (Platform.OS === 'android' || Platform.OS === 'ios') && !IS_TV && !IS_TABLET;
+const IS_STV = IS_TIZEN || IS_WEB_OS;
+const IS_WEB = Platform.OS === 'web' && !IS_STV;
+const IS_TV = Platform.isTV || IS_TIZEN || IS_WEB_OS;
+const IS_ANDROID_TV = Platform.OS === 'android' && IS_TV;
+const IS_TV_OS = Platform.OS === 'ios' && IS_TV;
+const IS_ANDROID = Platform.OS === 'android' && !IS_TV;
+const IS_IOS = Platform.OS === 'ios' && !IS_TV;
+
 const IS_TABLET = isTablet();
 const OS_NAME = getOS();
 const RUNTIME = getRuntime();
 const FORM_FACTOR = getFormFactor();
+
 
 export {
   IS_WEB,
