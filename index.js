b6f633df43e8909a5c7e7912f9bb300fab6c6495
Rokas <kasinskas.r@gmail.com>
2019-08-26 13:33:29 +0300

    istablet

--- index.js
+++ index.js
@@ -60,11 +60,11 @@ const getFormFactor = () => {
 const IS_TIZEN = /Tizen|tizen|TIZEN/.test(userAgent);
 const IS_WEB_OS = /Web0S|web0s|WEB0S|webos|WEBOS|WebOS/.test(userAgent);
 const IS_TV = Platform.isTV || IS_TIZEN || IS_WEB_OS;
-const IS_TABLET = isTablet() && !IS_TV;
-const IS_MOBILE =
-  (Platform.OS === "android" || Platform.OS === "ios") && !IS_TV && !IS_TABLET;
 const IS_STV = IS_TIZEN || IS_WEB_OS;
 const IS_WEB = Platform.OS === "web" && !IS_STV;
+const IS_TABLET = isTablet() && !IS_TV && !IS_WEB;
+const IS_MOBILE =
+  (Platform.OS === "android" || Platform.OS === "ios") && !IS_TV && !IS_TABLET;
 const IS_ANDROID_TV = Platform.OS === "android" && IS_TV;
 const IS_TV_OS = Platform.OS === "ios" && IS_TV;
 const IS_ANDROID = Platform.OS === "android" && !IS_TV;
